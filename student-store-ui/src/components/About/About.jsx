84ca4376a47898fcc31a4f1d531e8b1f13c606ec
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:05:00 -0700

    Update About.jsx

--- student-store-ui/src/components/About/About.jsx
+++ student-store-ui/src/components/About/About.jsx
@@ -1,1 +1,32 @@
+import React from 'react'
+import "./About.css"
 
+const About = () => {
+  return (
+    <div className='about' id='About'>
+        <div className='content'>
+            <h3>About Us</h3>
+            <div className='summary'>
+                <div className='text'>
+                    <p>The CodePath Student Store offers great products at great prices
+                        from a great team and for a great cause!
+                    </p>
+                    <p>
+                    We've searched far and wide for items that perk the interests
+                    of even the most eccentric students and decided to offer them all here in one place.
+                    </p>
+                    <p>
+                    All proceeds go towards bringing high quality CS education to 
+                    college students around the country.
+                    </p>
+                </div>
+            <div className='media'>
+                <img src="https://codepath-student-store-demo.surge.sh/assets/giant_codepath.6952ef57.svg" alt='codepath large' />
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
