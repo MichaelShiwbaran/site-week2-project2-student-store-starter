59823d539ac80c6ff2238bbbf1416a327c11f6f2
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:26:32 -0700

    Update ShoppingCartTotal.jsx

--- student-store-ui/src/components/ShoppingCartTotal/ShoppingCartTotal.jsx
+++ student-store-ui/src/components/ShoppingCartTotal/ShoppingCartTotal.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
 
+const ShoppingCartTotal = (props) => {
+let totalCost = 0
+let subTotal = 0
+props.shoppingCart.map((element) => {
+    subTotal += element.productCost
+})
+console.log(totalCost)
+totalCost = subTotal * 1.05
+
+  return (
+    <div>
+        <h1>Subtotal Cost: {subTotal.toFixed(2)} </h1>
+        <h1>Total Cost: {totalCost.toFixed(2)}</h1>
+    </div>
+  )
+}
+
+export default ShoppingCartTotal
