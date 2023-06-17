2d229e15b3106c0b4336a170d28a9bec5bb037a9
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:24:42 -0700

    Update ShoppingCart.jsx

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import ShoppingCartInfo from '../ShoppingCartInfo/ShoppingCartInfo'
 
+const ShoppingCart = (props) => {
+  console.log(props.shoppingCart)
+  return (
+    <div className='shopping-cart'>
+      {props.shoppingCart.map((element) => 
+        
+          <ShoppingCartInfo product = {element} />
+      )}
+
+    </div>
+  )
+}
+
+export default ShoppingCart
