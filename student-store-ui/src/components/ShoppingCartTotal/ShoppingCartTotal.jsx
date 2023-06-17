659dfc9de7e022e32647d720fa406162e06ced78
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:48:39 -0700

    Update ShoppingCartTotal.jsx

--- student-store-ui/src/components/ShoppingCartTotal/ShoppingCartTotal.jsx
+++ student-store-ui/src/components/ShoppingCartTotal/ShoppingCartTotal.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 const ShoppingCartTotal = (props) => {
 let totalCost = 0
 let subTotal = 0
+    //maps through each item in the cart to get the total cost
 props.shoppingCart.map((element) => {
     subTotal += element.productCost
 })
 console.log(totalCost)
-totalCost = subTotal * 1.05
+totalCost = subTotal * 1.05 //tax included
 
+    //prints out subtotal and total costs for cart items
   return (
     <div>
         <h1>Subtotal Cost: {subTotal.toFixed(2)} </h1>
