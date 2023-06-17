d2570edf335035974215fa031b072ff16f77bada
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:47:15 -0700

    Update ShoppingCart.jsx

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,6 +3,7 @@ import ShoppingCartInfo from '../ShoppingCartInfo/ShoppingCartInfo'
 
 const ShoppingCart = (props) => {
   console.log(props.shoppingCart)
+  // iterates through the shopping cart array and prints out the info for each item in the cart
   return (
     <div className='shopping-cart'>
       {props.shoppingCart.map((element) => 
