6dd880e4c2dc75625137d49c75edd9ef8e56d7a3
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:25:54 -0700

    Update ShoppingCartInfo.jsx

--- student-store-ui/src/components/ShoppingCartInfo/ShoppingCartInfo.jsx
+++ student-store-ui/src/components/ShoppingCartInfo/ShoppingCartInfo.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
 
+const ShoppingCartInfo = (props) => {
+    console.log(props.product)
+  return (
+    <div className='shopppingcart-info'>
+            <h3>Item: {props.product.productName}</h3>
+            <h3>Amount: {props.product.itemCount}</h3>
+            <h3>Price: {props.product.productPrice}</h3>
+            <h3>Cost: {props.product.productCost}</h3>
+    </div>  
+  )
+}
+
+export default ShoppingCartInfo
