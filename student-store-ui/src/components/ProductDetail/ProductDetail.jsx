ba13e00074acf25ab1a80fe105e886e37456eccf
MichaelShiwbaran <92387562+MichaelShiwbaran@users.noreply.github.com>
2023-06-16 19:14:51 -0700

    Update ProductDetail.jsx

--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -1,1 +1,33 @@
+import React from 'react'
+import "./ProductDetail.css"
+import ProductView from '../ProductView/ProductView'
+import Navbar from '../Navbar/Navbar'
+import Hero from '../Hero/Hero'
+import SubNavBar from '../SubNavbar/SubNavBar'
 
+const ProductDetail = (props) => {
+  console.log(props.products)
+  console.log(props.setIsFetching)
+  return (
+    <div className='product-detail'>
+          <Navbar />
+          <Hero />
+          <SubNavBar 
+      currentCategory = {props.currentCategory}
+      setCurrentCategory = {props.setCurrentCategory}
+      handleSearchValueChange = {props.handleSearchValueChange}
+      searchValue = {props.searchValue}
+      />
+          <ProductView 
+          product = {props.products}
+          setIsFetching = {props.setIsFetching}
+          isFetching = {props.isFetching}
+          setProducts = {props.setProducts}
+          shoppingCart = {props.shoppingCart}
+      setShoppingCart = {props.setShoppingCart}
+          />
+      </div>
+  )
+}
+
+export default ProductDetail
